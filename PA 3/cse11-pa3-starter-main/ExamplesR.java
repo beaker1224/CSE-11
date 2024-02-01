3ae665c356e1d1dc08a8aa7f99e3e91b95a9415a
beaker1224 <qs1213489684@gmail.com>
2024-01-31 20:40:56 -0800

    main

--- PA 3/cse11-pa3-starter-main/ExamplesR.java
+++ PA 3/cse11-pa3-starter-main/ExamplesR.java
@@ -1,13 +1,29 @@
 class R{
-    String first;
-    R second;
+    String content;
+    R comment;
 //constructor---------------------------
-    R(String first, R second){
-        this.first = first;
-        this.second = second;
+    R(String content, R replyTo){
+        this.content = content;
+        this.comment = replyTo;
     }
 }
 
 class ExamplesR{
-    
+    R ex1 = new R("nice", null);
+    R ex2 = new R("yes it is nice", ex1);
+    R ex3 = new R("I don't think so", ex2);
+    String theFirstComment = ex3.comment.comment.content;
 }
+//Answer to Question 1
+/*I am able to construct an example R object, my first example
+is "ex1", which the second field in the constructor is null,
+since I don't known what should I put there yet. Then "ex2" is like
+a second person commenting on the first person called "ex2", then the 
+second field I can put "ex1" since it is a R object.
+"theFirstComment" is an explainition of how I can get the first person's comment
+from the third person "ex3"
+*/
+//Answer to Question 2
+/*
+
+ */
