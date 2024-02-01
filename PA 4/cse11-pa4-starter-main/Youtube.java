69b82b5422f028123f573893410e943c182a394d
beaker1224 <qs1213489684@gmail.com>
2024-02-01 09:54:16 -0800

    main

--- PA 4/cse11-pa4-starter-main/Youtube.java
+++ PA 4/cse11-pa4-starter-main/Youtube.java
@@ -1,1 +1,27 @@
 import tester.*;
+
+class User{
+    String Username;
+    String displayName;
+
+    interface Comment{
+        public boolean isCommentByAuthor(User author);
+        public int totalLikes();
+        public String unrollCommentThread();
+    }
+
+}
+
+class VideoComment implements Comment{
+    String text;
+    int likes;
+    int replies;
+    User author;
+
+VideoComment(String text, int likes, int replies, User author){
+    this.text = text;
+    this.likes = likes;
+    this.replies = replies;
+    this.author = author;
+}
+}
