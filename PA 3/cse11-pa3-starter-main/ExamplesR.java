908a6acb0229162b23294d48ae3ed70d08cf9b9e
beaker1224 <qs1213489684@gmail.com>
2024-01-31 21:16:13 -0800

    main

--- PA 3/cse11-pa3-starter-main/ExamplesR.java
+++ PA 3/cse11-pa3-starter-main/ExamplesR.java
@@ -19,11 +19,49 @@ class ExamplesR{
 is "ex1", which the second field in the constructor is null,
 since I don't known what should I put there yet. Then "ex2" is like
 a second person commenting on the first person called "ex2", then the 
-second field I can put "ex1" since it is a R object.
+second field I can put "ex1" since it is a R object. The null in the
+first R object means this is the first one, and I have nothing to refer.
 "theFirstComment" is an explainition of how I can get the first person's comment
 from the third person "ex3"
 */
-//Answer to Question 2
-/*
 
- */
+//Answer to Question 2
+class VideoComment{
+    String text;
+    int likes;
+    int replies;
+    VideoComment(String text, int likes, int replies){
+        this.text = text;
+        this.likes = likes;
+        this.replies = replies;
+    }
+}
+
+class CommentReply{
+    VideoComment replyTo;
+    String text;
+    int likes;
+    int replies;
+    CommentReply(VideoComment replyTo, String text, int likes, int replies){
+        this.replyTo = replyTo;
+        this.text = text;
+        this.likes = likes;
+        this.replies = replies;
+    }
+}
+
+class Examples2{
+    CommentReply p1 = new CommentReply(null, "this sounds great", 0, 0);
+    CommentReply p2 = new CommentReply(p1.replyTo, "then should we go tomorrow?", 0, 0);
+    /*
+     * here we cannot construct an example of a reply to a reply
+     * to a Comment, because we now requires a vidoComment object in one
+     * of the CommentReply object's field but in the example I provided above,
+     * "p1" itself will be too much for the VideoComment portion of CommentReply
+     * from "p2", thus the only solution is to use "p1.replyTo" because it is a
+     * VideoComment object. In this case, all the new CommentReply will be pointed 
+     * towards the VideoComment itself, instead of point to the reply (or replies)
+     * of the VideoComment
+     */
+}
+
