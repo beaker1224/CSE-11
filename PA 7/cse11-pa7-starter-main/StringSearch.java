5ad0a38bd69a076413ce4f375217d3b8f9efaec8
beaker1224 <qs1213489684@gmail.com>
2024-03-05 21:00:53 -0800

    main

--- PA 7/cse11-pa7-starter-main/StringSearch.java
+++ PA 7/cse11-pa7-starter-main/StringSearch.java
@@ -11,5 +11,66 @@ class FileHelper {
         }
     }
 }
+
 class StringSearch{
+  public static void main(String[] args) throws IOException{
+//to get the contents
+    String[] contents = Files.readString(Paths.get(args[0])).split("\n");
+    for (String line : contents) {
+      System.out.println(line);
+    }
+//to get the conditions
+    String[] conditions = args[1].split("&");// 0 to 5 for the index
+   
+    int length, greater, less, first, last;
+    String contains, starts, ends, replace;
+
+    for(String s:conditions){
+      if(s.contains("length")){
+        length = Integer.parseInt(s.substring(7));
+      }
+      if(s.contains("greater")){
+        greater = Integer.parseInt(s.substring(8));
+      }
+      if(s.contains("less")){
+        less = Integer.parseInt(s.substring(6));
+      }
+      if(s.contains("contains")){
+        for(String c:contents){
+          if(c.contains(s.substring(10))){
+            System.out.println(c);
+          }
+        }
+      }
+      if(s.contains("starts")){
+        starts = s.substring(8);
+      }
+      if(s.contains("ends")){
+        ends = s.substring(6);
+      }
+      if(s.contains("not")){
+
+      }
+    }
+//to get the operations  
+    String[] transforms = args[2].split("&");
+    for(String s:transforms){
+      if(s.contains("upper")){
+        
+    }
+      if(s.contains("lower")){
+        
+    }
+      if(s.contains("first")){
+        first = Integer.parseInt(s.substring(7));
+    }
+      if(s.contains("last")){
+        last = Integer.parseInt(s.substring(6));
+    }
+      if(s.contains("replace")){
+        String[] replaces = s.substring(9).split(";");
+        
+    }
+  }
+}
 }
--- PA 7/cse11-pa7-starter-main/StringSearchMilestone1.java
+++ PA 7/cse11-pa7-starter-main/StringSearchMilestone1.java
@@ -12,4 +12,11 @@ class FileHelper {
     }
 }
 class StringSearch{
+    public static void main(String[] args) throws IOException{
+  //to get the contents
+      String[] contents = Files.readString(Paths.get(args[0])).split("\n");
+      for (String line : contents) {
+        System.out.println(line);
+      }
+    }
 }
